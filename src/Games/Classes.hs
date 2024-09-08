ecc7bc25b45ab27c13967b33a4cf51eba23c29bb
augusto peres <augustoluisperes@gmail.com>
2024-09-08 19:36:22 +0100

    refactor

--- src/Games/Classes.hs
+++ src/Games/Classes.hs
@@ -1,45 +1,47 @@
 {-# LANGUAGE FunctionalDependencies #-}
-module Games.Classes(
-  AdversarialGame,
-  step,
-  availableActions,
-  isFinal,
-  currentPlayer,
-  getRandomAction,
-  playRandomAction,
-  playRandomGame,
-  winner,) where
 
-import Control.Monad.State (State, get, put)
-import System.Random (StdGen, split)
+module Games.Classes
+  ( AdversarialGame
+  , step
+  , availableActions
+  , isFinal
+  , currentPlayer
+  , getRandomAction
+  , playRandomAction
+  , -- , playRandomGame
+    winner
+  ) where
 
-import           Utils.Utils (choice)
+import           Control.Monad.State (State, get, put)
+import           System.Random       (StdGen)
 
-class (Eq p) => AdversarialGame g a p | g -> a p where
+import           Utils.Utils         (choice)
+
+class (Eq p) =>
+      AdversarialGame g a p
+  | g -> a p
+  where
   step :: g -> a -> g
   availableActions :: g -> [a]
   currentPlayer :: g -> p
   isFinal :: g -> Bool
   winner :: g -> Maybe p
-
-  getRandomAction :: StdGen -> g -> Maybe a
-  getRandomAction gen game =
-    if null $ availableActions game
-      then Nothing
-      else Just action
-    where (_, action) = choice (availableActions game) gen
-
+  getRandomAction :: g -> State StdGen (Maybe a)
+  getRandomAction game =
+    if null actions
+      then return Nothing
+      else get >>= \gen ->
+             let (newGen, action) = choice actions gen
+              in put newGen >> return (Just action)
+    where
+      actions = availableActions game
   playRandomAction :: g -> State StdGen g
   playRandomAction game = do
-    gen <- get
-    case getRandomAction gen game of
-      Nothing -> return game
-      Just action -> do
-        let newGame = step game action
-        let (gen1, _) = split gen
-        put gen1
-        return newGame
-    
+    getRandomAction game
+      >>= (\a ->
+             case a of
+               Nothing     -> return game
+               Just action -> return $ step game action)
   playRandomGame :: g -> State StdGen g
   playRandomGame game = do
     if isFinal game
@@ -47,5 +49,3 @@ class (Eq p) => AdversarialGame g a p | g -> a p where
       else do
         newGame <- playRandomAction game
         playRandomGame newGame
-    
-    
--- src/Games/TicTacToe.hs
+++ src/Games/TicTacToe.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
-module Games.TicTacToe (
-    TicTacToe,
-    Player(..),
-    Action,
-    initialGame
-) where
+
+module Games.TicTacToe
+  ( TicTacToe
+  , Player(..)
+  , Action
+  , initialGame
+  ) where
 
 {--
 
@@ -23,17 +24,22 @@ ghci> fst $ runState s (mkStdGen 1)
 TicTacToe {board = [Just O,Just X,Just O,Just O,Just X,Just X,Just O,Nothing,Just X], player = X}
 
 |--}
+import           Games.Classes (AdversarialGame, availableActions,
+                                currentPlayer, isFinal, step, winner)
 
-import Games.Classes (AdversarialGame, step, availableActions, isFinal, winner, currentPlayer)
+data Player
+  = X
+  | O
+  deriving (Eq, Show)
 
-data Player = X | O deriving (Eq, Show)
 type Action = Int
+
 type Board = [Maybe Player]
 
-data TicTacToe = TicTacToe {
-    board :: Board,
-    player :: Player
-} deriving (Eq, Show)
+data TicTacToe = TicTacToe
+  { board  :: Board
+  , player :: Player
+  } deriving (Eq, Show)
 
 initialGame :: TicTacToe
 initialGame = TicTacToe (replicate 9 Nothing) X
@@ -43,38 +49,36 @@ nextPlayer X = O
 nextPlayer O = X
 
 instance AdversarialGame TicTacToe Action Player where
-    step (TicTacToe b p) action =
-      case b !! action of
-        Nothing -> TicTacToe newBoard (nextPlayer p)
-        Just _ -> TicTacToe b p
-      where newBoard = [f i mark | (i, mark) <- zip [0..] b]
-            f i mark = if i == action then Just p else mark
-
-    availableActions (TicTacToe b _) = [i | (i, Nothing) <- zip [0..8] b]
-
-    currentPlayer = player
-
-    isFinal game =
-      any (\x -> isWinningCombination x (board game)) [X, O]
-
-    winner (TicTacToe b _) =
-      if isWinningCombination X b
-        then Just X
-        else if isWinningCombination O b
-                then Just O
-                else Nothing
+  step (TicTacToe b p) action =
+    case b !! action of
+      Nothing -> TicTacToe newBoard (nextPlayer p)
+      Just _  -> TicTacToe b p
+    where
+      newBoard = [f i mark | (i, mark) <- zip [0 ..] b]
+      f i mark =
+        if i == action
+          then Just p
+          else mark
+  availableActions (TicTacToe b _) = [i | (i, Nothing) <- zip [0 .. 8] b]
+  currentPlayer = player
+  isFinal game = any (\x -> isWinningCombination x (board game)) [X, O]
+  winner (TicTacToe b _) =
+    if isWinningCombination X b
+      then Just X
+      else if isWinningCombination O b
+             then Just O
+             else Nothing
 
 isWinningCombination :: Player -> Board -> Bool
 isWinningCombination p b = any (all (== Just p)) winningIndices
   where
-    winningIndices = [
-        [b !! 0, b !! 1, b !! 2],
-        [b !! 3, b !! 4, b !! 5],
-        [b !! 6, b !! 7, b !! 8],
-        [b !! 0, b !! 3, b !! 6],
-        [b !! 1, b !! 4, b !! 7],
-        [b !! 2, b !! 5, b !! 8],
-        [b !! 0, b !! 4, b !! 8],
-        [b !! 2, b !! 4, b !! 6]
+    winningIndices =
+      [ [b !! 0, b !! 1, b !! 2]
+      , [b !! 3, b !! 4, b !! 5]
+      , [b !! 6, b !! 7, b !! 8]
+      , [b !! 0, b !! 3, b !! 6]
+      , [b !! 1, b !! 4, b !! 7]
+      , [b !! 2, b !! 5, b !! 8]
+      , [b !! 0, b !! 4, b !! 8]
+      , [b !! 2, b !! 4, b !! 6]
       ]
-    
